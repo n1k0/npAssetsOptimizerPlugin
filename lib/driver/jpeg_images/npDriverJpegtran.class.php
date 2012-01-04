a17d1413e904bc7dc73065c03c21ed35ff11d96e
Jeremy <jeremy.benoist@gmail.com>
2012-01-04 11:31:47 +0100

    Fix jpg optimization
    I used the same way of png copy file. With the old way, I don't know why, the mime type was changed to "application/octet-stream" and it was impossible to view the image again.
    
    I also added a -perfect option which check that -copy and -optimize job is well done.

--- lib/driver/jpeg_images/npDriverJpegtran.class.php
+++ lib/driver/jpeg_images/npDriverJpegtran.class.php
@@ -21,18 +21,24 @@ class npDriverJpegtran extends npDriverBase
     {
       throw new LogicException('JPEG optimization only support file replacement atm');
     }
-    
+
     exec('which jpegtran', $output, $return);
-    
+
     if (!count($output) || $return > 0)
-    {      
+    {
       throw new RuntimeException('The jpegtran program is not available nor accessible by php on your system');
     }
-    
-    exec(sprintf('jpegtran -copy none -optimize %s 2>/dev/null', escapeshellarg($file)), $output, $return);
+    $tmpFile = sprintf('%s.tmp', $file);
 
-    $this->replaceFile($file, $output);
-    
+    exec(sprintf('jpegtran -copy none -optimize -perfect %s > %s', escapeshellarg($file), escapeshellarg($tmpFile)), $output, $return);
+
+    if (file_exists($tmpFile) && filesize($tmpFile) < filesize($file))
+    {
+      copy($tmpFile, $file);
+    }
+
+    unlink($tmpFile);
+
     return $file;
   }
 }
